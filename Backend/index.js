c5f6ff6e0a7c321d2502f00f71afc99d47f01e11
Krish Srivastava <82054542+retr0-kernel@users.noreply.github.com>
2023-02-25 06:40:04 +0530

    Add files via upload

---
+++ Backend/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+var http = require("http");
+const app = express();
+const port = process.env.PORT || 5000;
+var server = http.createServer(app);
+var io = require("socket.io")(server);
+
+app.use(express.json());
+var clients = {};
+
+io.on("connection", (socket) => {
+  console.log("connetetd");
+  console.log(socket.id, "has joined");
+  socket.on("signin", (id) => {
+    console.log(id);
+    clients[id] = socket;
+    console.log(clients);
+  });
+  socket.on("message", (msg) => {
+    console.log(msg);
+    let targetId = msg.targetId;
+    if (clients[targetId]) clients[targetId].emit("message", msg);
+  });
+});
+
+server.listen(port, "0.0.0.0", () => {
+  console.log("server started");
+});
